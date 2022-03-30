31207b25e93257d0cdd6f17b9698c1046179a7e5
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2022-03-30 10:06:03 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using proj.Entities;
+
+namespace proj
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+          Clients clienteUm = new Clients{Name = "Mayara", Email = "mayara@hotmail.com"};
+          Clients clienteDois = new Clients{Name = "may", Email = "may@hotmail.com"};
+
+            System.Console.WriteLine(clienteUm.Equals(clienteDois));
+       
+        }
+
+
+    }
+}
